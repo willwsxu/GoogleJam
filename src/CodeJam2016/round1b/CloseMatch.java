a6f8054feef39fdb5e462a32e401e06ce43da5fb
willwsxu <willwsxu@gmail.com>
2017-04-18 17:15:47 -0500

    close match refactor

--- src/CodeJam2016/round1b/CloseMatch.java
+++ src/CodeJam2016/round1b/CloseMatch.java
@@ -39,8 +39,6 @@ public class CloseMatch {
     
     void greedy(String C, String J)
     {
-        int c=0;
-        int j=0;
         int cmp=0; // -1 c<J, 0 same, 1 C>J
         StringBuilder newC=new StringBuilder(), newJ=new StringBuilder();
         int len = min(C.length(), J.length());
@@ -54,11 +52,11 @@ public class CloseMatch {
         for (int i=0; i<len; i++) {
             int lookahead=0;
             if (i<len-1) {
-                if (C.charAt(i+c+1)!='?' && J.charAt(i+j+1)!='?') {
-                    lookahead = C.charAt(i+c+1)-J.charAt(i+j+1);
+                if (C.charAt(i+1)!='?' && J.charAt(i+1)!='?') {
+                    lookahead = C.charAt(i+1)-J.charAt(i+1);
                 }
             }
-            if (C.charAt(i+c)=='?' && J.charAt(i+j)=='?') {
+            if (C.charAt(i)=='?' && J.charAt(i)=='?') {
                 if (cmp<0) {
                     newC.append('9');
                     newJ.append('0');
@@ -81,13 +79,13 @@ public class CloseMatch {
                     }
                 }
             }
-            else if (C.charAt(i+c)=='?') {
+            else if (C.charAt(i)=='?') {
                 if (cmp<0) {
                     newC.append('9');
                 } else if (cmp>0) {
                     newC.append('0');                  
                 } else {
-                    char ch=J.charAt(i+j);
+                    char ch=J.charAt(i);
                     if ( lookahead> 5 && ch>'0' ) { // ?7 vs 11
                         newC.append((char)( ch-1)); 
                         cmp=-1;
@@ -99,15 +97,15 @@ public class CloseMatch {
                     else
                         newC.append(ch);                   
                 }   
-                newJ.append(J.charAt(i+j));     
+                newJ.append(J.charAt(i));     
             }
-            else if (J.charAt(i+j)=='?') {
+            else if (J.charAt(i)=='?') {
                 if (cmp<0) {
                     newJ.append('0');
                 } else if (cmp>0) {
                     newJ.append('9');                    
                 } else {
-                    char ch=C.charAt(i+c);
+                    char ch=C.charAt(i);
                     if ( lookahead> 5 && ch<'9' ) {  // 07 vs ?1
                         //out.println(newJ.toString());
                         newJ.append((char)(ch+1));  // need cast to get char instead of int
@@ -120,14 +118,14 @@ public class CloseMatch {
                     } else
                         newJ.append(ch);                     
                 }
-                newC.append(C.charAt(i+c));
+                newC.append(C.charAt(i));
             }
             else {
                 if (cmp==0 ) {
-                    cmp = C.charAt(i+c)>J.charAt(i+j)? 1:-1;
+                    cmp = C.charAt(i)>J.charAt(i)? 1:-1;
                 }
-                newC.append(C.charAt(i+c));
-                newJ.append(J.charAt(i+j));  
+                newC.append(C.charAt(i));
+                newJ.append(J.charAt(i));  
             }
         }
         out.println(newC.toString()+" "+newJ.toString());
@@ -136,9 +134,9 @@ public class CloseMatch {
     static Scanner sc = new Scanner(System.in);  
     public static void main(String[] args)  
     {
-        googlejam.ContestHelper.redirect("out.txt");
-        sc = googlejam.ContestHelper.getFileScanner("closematch-s.in.txt");
-        //sc = googlejam.ContestHelper.getFileScanner("closematch-t.txt");
+        //googlejam.ContestHelper.redirect("out.txt");
+        //sc = googlejam.ContestHelper.getFileScanner("closematch-s.in.txt");
+        sc = googlejam.ContestHelper.getFileScanner("closematch-t.txt");
         
         int TC = sc.nextInt(); // 1 to 100
         for (int i=0; i<TC; i++) {
