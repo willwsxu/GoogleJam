ae799fbd5fb7a5fa17351426d1b853305d96f56f
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-04-20 20:52:27 -0500

    senate evacuation greedy2 priority queue

--- src/CodeJam2016/round1c/SenateEvac.java
+++ src/CodeJam2016/round1c/SenateEvac.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class SenateEvac {
@@ -34,18 +35,19 @@ public class SenateEvac {
             return (char)('A'+i);
         return 'Z';
     }
-    public void greedy(Integer P[])
+        
+    List<String> plan = new ArrayList<>();
+    Comparator<Party> cmp = (p1,p2)->p2.n-p1.n;
+    public void greedy(int P[])
     {
         int total=0;
         int N=P.length;
-        List<String> plan = new ArrayList<>();
         List<Party> senate = new ArrayList<>();
         for (int i=0; i<N; i++) {
             total+=P[i];
             senate.add(new Party(getAlpha(i), P[i]));
         }
         
-        Comparator<Party> cmp = (p1,p2)->p2.n-p1.n;
         while (total-->0) {
             Collections.sort(senate, cmp);
             // take one from the top
@@ -60,9 +62,41 @@ public class SenateEvac {
             }      
             plan.add(sb.toString());
         }
+    }
+    
+    void print()
+    {
         for (int i=0; i<plan.size(); i++)
             out.print(plan.get(i)+" ");
-        out.println();
+        out.println();        
+    }
+    private void takeone(PriorityQueue<Party> senate, StringBuilder sb)
+    {
+        Party p = senate.poll();
+        sb.append(p.a);
+        p.decreemnt();
+        if (p.n>0)
+            senate.add(p);        
+    }
+    public void greedy2(int P[])  // implement with PriorityQueue
+    {
+        PriorityQueue<Party> senate = new PriorityQueue<>(26, cmp);
+        int total=0;
+        int N=P.length;
+        for (int i=0; i<N; i++) {
+            total+=P[i];
+            senate.add(new Party(getAlpha(i), P[i]));
+        }
+        while (!senate.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            takeone(senate, sb);
+            if (--total==1 || total>2) {
+                takeone(senate, sb);
+                total--;
+            }
+            plan.add(sb.toString());
+        }
+        print();
     }
     
     static Scanner sc = new Scanner(System.in);  
@@ -74,11 +108,11 @@ public class SenateEvac {
         int TC = sc.nextInt(); // 1 to 50
         for (int i=0; i<TC; i++) {
             int N = sc.nextInt();  // 2 to 26
-            Integer P[] = new Integer[N];   // 1 to 1000
+            int P[] = new int[N];   // 1 to 1000
             for (int j=0; j<N; j++)
                 P[j] = sc.nextInt();
             out.print("Case #"+(i+1)+": ");
-            new SenateEvac().greedy(P);
+            new SenateEvac().greedy2(P);
         }
     }
 }
