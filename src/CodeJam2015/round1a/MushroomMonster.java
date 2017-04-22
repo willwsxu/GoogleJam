92157eca4f2c7f843a590769cc2336fae2150305
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-04-21 22:18:32 -0500

    fix package

--- src/CodeJam2015/round1a/MushroomMonster.java
+++ src/CodeJam2015/round1a/MushroomMonster.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package CodeJam2015.round1a;
 
 import static java.lang.Integer.max;
 import static java.lang.Integer.min;
