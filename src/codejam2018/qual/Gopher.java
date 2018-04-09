d75ac83c5c4a012e7d0bbf97641d9d9983c71b92
willwsxu <willwsxu@gmail.com>
2018-04-09 10:39:51 -0500

    refactor to follow SOLID principles

--- src/codejam2018/qual/Gopher.java
+++ src/codejam2018/qual/Gopher.java
@@ -32,73 +32,43 @@ public class Gopher {
         
     int[] addCell(List<int[]> cells, int i1, int j1, int A) {  
         moves++;
-        //AI, guess next move
-        if (grid==null) {
-            cells.add(new int[]{i1, j1});
-            if (topleft[0]==0) {  // make it at least 3x3
-                topleft[0]=i1;
-                bottomright[0]=i1+2;
-                topleft[1]=j1;
-                bottomright[1]=j1+2;
-                return new int[]{i1+2, j1+2};
-            } else {
-                if (i1<topleft[0])
-                    topleft[0]=i1;
-                if (j1<topleft[1])
-                    topleft[1]=j1;
-                if (i1>bottomright[0])
-                    bottomright[0]=i1;
-                if (j1>bottomright[1])
-                    bottomright[1]=j1;
-                if (cells.size()<4)
-                    return new int[]{topleft[0]+1, bottomright[1]-1};
-            }
-        } else
-            grid[i1-topleft[0]][j1-topleft[1]]=true;
+        cells.add(new int[]{i1,j1});
+        grid[i1-topleft[0]][j1-topleft[1]]=true;
         return choose(cells, A);
     }
     boolean grid[][]=null;
+    int MaxCount=9;
+    int count(int r, int c)
+    {
+        int cnt=0;
+        if (grid[r-1][c-1])
+            cnt++;
+        if ( grid[r-1][c] )
+            cnt++;
+        if ( grid[r-1][c+1] )
+            cnt++;
+        if ( grid[r][c-1] )
+            cnt++;
+        if ( grid[r][c] )
+            cnt++;
+        if ( grid[r][c+1] )
+            cnt++;
+        if (grid[r+1][c-1] )
+            cnt++;
+        if (grid[r+1][c] )
+            cnt++;
+        if (grid[r+1][c+1])
+            cnt++;
+        return cnt;
+    }
     int[] choose(List<int[]> cells, int A)
     {
-        int cols=0;
-        int rows=0;
-        if (grid==null) {
-            cols=bottomright[1]-topleft[1]+1;
-            rows=bottomright[0]-topleft[0]+1;
-            int Area=rows*cols;
-            if (Area>=A) {
-                //System.err.println("rectangle "+rows+" "+cols);
-                grid=new boolean[rows][cols];
-                for (int[] c: cells)
-                    grid[c[0]-topleft[0]][c[1]-topleft[1]]=true;
-            }
-        } else {
-            rows=grid.length;
-            cols=grid[0].length;
+        int k=1;
+        for (k=1; k<grid[0].length-1; k++) {
+            if (count(1,k)!=9)
+                break;
         }
-        if ( grid!=null) {
-            rows -=2;
-            cols -=2;            
-        }
-        int i=topleft[0]+rand.nextInt(rows);
-        int j=topleft[1]+rand.nextInt(cols);
-        if (grid!=null) {
-            i++;
-            j++;
-            int r=i-topleft[0];
-            int c=j-topleft[1];
-            int loop=9;
-            while (grid[r-1][c-1] && grid[r-1][c] && grid[r-1][c+1] && grid[r][c-1]
-                     && grid[r][c] && grid[r][c+1] && grid[r+1][c-1] && grid[r+1][c] && grid[r+1][c+1]) {
-                i=topleft[0]+rand.nextInt(rows)+1;
-                j=topleft[1]+rand.nextInt(cols)+1;
-                r=i-topleft[0];
-                c=j-topleft[1];
-                if (loop--==0)
-                    break;
-            }
-        }
-        return new int[]{i, j};
+        return new int[]{2, k+1};
     }
     int topleft[]=new int[]{1,1};
     int bottomright[]=new int[]{0,0};
